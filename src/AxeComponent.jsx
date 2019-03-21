4de0e1c35cc62013ea2b11196862665570c70114
entegral <e2robby@gmail.com>
2019-03-21 09:44:37 -0700

    add styling

--- src/AxeComponent.jsx
+++ src/AxeComponent.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {mockData} from './mockApiData';
 
 const DisplayAxeResult = (props) =>{
-    console.log('props',props);
     return(
     <div>
       {props.axeResult.map((testedPage, index)=>
@@ -32,12 +31,34 @@ class AxeComponent extends React.Component {
     return this.setState({ axeResult: mockData }) };
 
   render(){
-    return (
-      <React.Fragment>
-        <button onClick={this.axeApi}>Call API </button>
-        {this.state.axeResult ? <DisplayAxeResult axeResult = {this.state.axeResult} /> : <div>No data in state</div>  }
+    const divStyle = {
+      marginTop: '15px',
+      marginBottom: '15px',
+      padding: '15px',
+      width: '400px',
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      border: '1px solid grey',
+      borderRadius: '4px',
+      backgroundColor: 'lightgrey',
+      boxShadow: '5px 5px 15px grey'
+    }
 
-    </React.Fragment>
+    const buttonStyle = {
+      width: '33%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginBottom: '15px',
+      backgroundColor: '#96d7f7',
+      border: '1px solid #52707f',
+      borderRadius: '2px',
+    }
+
+    return (
+      <div style = {divStyle}>
+        <button style={buttonStyle} onClick={this.axeApi}>Call API </button>
+        {this.state.axeResult ? <DisplayAxeResult axeResult = {this.state.axeResult} /> : <div>No data in state</div>  }
+      </div>
     )
   };
 
